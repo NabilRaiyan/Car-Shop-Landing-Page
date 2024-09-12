0ea832006f2c4e11a8263006ab50744271f3fe5f
NabilRaiyan <rraiyan77@gmail.com>
2024-09-12 20:56:27 +0600

    creatign navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+'use client';
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 const Nav = () => {
   return (
     <div>
-      
+        <nav className="flex-between pt-3 w-full mb-16">
+            <Link href="/" className="flex gap-2 flex-center">
+                <Image alt="promptopia logo" width={30} height={30} className="object-contain" src="/assets/images/logo.svg" />
+            </Link>
+        </nav>
     </div>
   )
 }
