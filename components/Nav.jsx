8dac86a121315a3523266c12e195cdb449bd56b8
NabilRaiyan <rraiyan77@gmail.com>
2024-09-14 21:57:47 +0600

    creating google  authentication

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
+// Navbar component
 const Nav = () => {
     const isUserLoggedIn = true;
     const [providers, setProviders] = useState(null);
