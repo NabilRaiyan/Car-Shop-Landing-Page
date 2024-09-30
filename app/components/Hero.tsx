5a4f1b40838b4d825230c2bc187cbf38d4d968f6
NabilRaiyan <rraiyan77@gmail.com>
2024-09-30 07:25:15 +0600

    creating Hero, CustomButton

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image"
+"use client"
 
+import Image from "next/image"
+import CustomButton from "./CustomButton"
+
+const handleScroll = () => {
+
+}
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+                Find, book or rend a car -- quickly and easily
+            </h1>
+
+            <p className="hero__subtitle">
+                Streamline your car rental experience with our effortless booking process.
+            </p>
+
+            <CustomButton title = "Explore cars" 
+                          containerStyles = "bg-primary-blue text-white rounded-full mt-10"
+                          handleClick = {handleScroll} />
+        </div>
     </div>
   )
 }
