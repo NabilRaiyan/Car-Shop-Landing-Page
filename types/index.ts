6b2841c3342d5fd6340fe48641d3c92721c254a8
NabilRaiyan <rraiyan77@gmail.com>
2024-09-30 08:08:32 +0600

    creating Navbar, Footer

--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,7 @@ import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
     title: String,
-    btnType: String,
+    btnType?: "button" | "submit",
     containerStyles?: String,
     handleClick?: MouseEventHandler<HTMLButtonElement>;
 
